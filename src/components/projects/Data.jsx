9df7dffbb2f3cdac85fc088c0a5ec443b166b4e0
jcasare <jerry.asare@gtnllc.com>
2026-02-04 04:30:31 -0500

    projects update

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -9,8 +9,8 @@ export const projectsData = [
   {
     id: 2,
     image: '/assets/ICGC.PNG',
-    title: 'ICGC Prayer Temple',
-    category: 'I',
+    title: 'Elite EnergyRig',
+    category: 'E',
     link: 'https://teal-parrot-732312.hostingersite.com/',
   },
   // {
@@ -50,10 +50,53 @@ export const projectsData = [
   },
   {
     id: 8,
-    image: '/assets/ltepos.PNG',
-    title: 'Lte POS',
-    category: 'L',
-    link: 'https://ltepos.com/',
+    image: '/assets/ataya-tea.png',
+    title: 'Ataya Tea',
+    category: 'A',
+    link: 'https://atayatea.com/',
+  },
+
+  {
+    id: 9,
+    image: '/assets/iscript-sys.png',
+    title: 'iScript Systems',
+    category: 'I',
+    link: 'https://iscriptsystems.com/',
+  },
+  {
+    id: 10,
+    image: '/assets/aim-building.png',
+    title: 'AIM Building Materials',
+    category: 'A',
+    link: 'https://www.aimbuildingmaterials.com/',
+  },
+  {
+    id: 11,
+    image: '/assets/bbb-int.png',
+    title: 'BBB Interventions',
+    category: 'B',
+    link: 'https://bbbinterventions.com/',
+  },
+  {
+    id: 12,
+    image: '/assets/fortiffi.png',
+    title: 'Fortiffi',
+    category: 'F',
+    link: 'https://app.fortiffi.com/affiliate-onboarding',
+  },
+  {
+    id: 13,
+    image: '/assets/global-trav.png',
+    title: 'Global Travel Network',
+    category: 'G',
+    link: 'https://globaltrips.com/login',
+  },
+  {
+    id: 14,
+    image: '/assets/roma-resources.png',
+    title: 'Roma Resources',
+    category: 'R',
+    link: 'https://romaresources.org/',
   },
 ];
 
