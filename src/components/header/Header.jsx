95b8067127adffc72f0e9d1bc6d0366f10d5e0fd
Jerry Comforter Asare <jerry.asare@gtnllc.com>
2026-02-05 10:29:09 +0000

    fix homepage nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -48,7 +48,7 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo">
+        <a href="/" className="nav__logo">
           &lt; iJay /&gt;
         </a>
         <div className={Toggle ? 'nav__menu show-menu' : 'nav__menu'}>
